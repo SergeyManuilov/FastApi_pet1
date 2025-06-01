d63289801fd505d995e30cd51636553a93387d68
GrindDevice <sergomanuilov@gmail.com>
2025-06-01 20:01:35 +0300

    commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("база очищена")
+    await create_tables()
+    print("база готова")
+    yield
+    print("выключение")
+
+
+app =FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+if __name__ == '__main__':
+    from uvicorn import run
+    run(app)
+
